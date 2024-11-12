35bb640378fc7394d1d8cc23be56db44d1498ecd
HendryASz/Hen.git <henhen56566565@gmail.com>
2024-11-12 20:56:03 +0700

    tambahan

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,27 @@
+// import { NgModule } from '@angular/core';
+// import { RouterModule } from '@angular/router';
+// import { routes } from './routes';
+
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes, { useHash: true } )],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
+
+
+
+
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { routes } from './routes';
 
-
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true } )],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }  // Gunakan HashLocationStrategy
+  ]
 })
 export class AppRoutingModule { }
-
-
-
-
