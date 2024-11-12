45386324b2cfe74d22b1e5ea6f50c8647c08056e
HendryASz/Hen.git <henhen56566565@gmail.com>
2024-11-12 21:26:13 +0700

    coba souce code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,3 @@
-// import { NgModule } from '@angular/core';
-// import { RouterModule } from '@angular/router';
-// import { routes } from './routes';
-
-
-// @NgModule({
-//   imports: [RouterModule.forRoot(routes, { useHash: true } )],
-//   exports: [RouterModule]
-// })
-// export class AppRoutingModule { }
 
 
 
